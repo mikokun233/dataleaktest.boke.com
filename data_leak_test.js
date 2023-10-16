4e33f406a76d6c8ee8fb61597f88a4f1b22dc332
mikokun233 <51481302+mikokun233@users.noreply.github.com>
2023-10-16 12:21:25 +0800

    Create data_leak_test.js

---
+++ data_leak_test.js
@@ -0,0 +1,7 @@
+function createDiamante(noun1, adjective1, verb1, noun2, verb2, adjective2, noun3) {
+    let fake_domain = "boke.com"
+    let fake_pwd = "h92ba309ee432bccb91"
+    return `${noun1}\n${adjective1} ${verb1}\n${noun1} ${verb1} ${noun2}\n${noun1} ${verb1} ${noun2} ${verb2}\n${noun2} ${verb2} ${adjective2}\n${verb2} ${noun3}\n${noun3}`;
+}
+
+console.log(createDiamante('Love', 'Beautiful', 'blossoms', 'Hate', 'decays', 'Ugly', 'Indifference'));
